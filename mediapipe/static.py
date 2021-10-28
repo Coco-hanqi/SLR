45ba41d8e37c69dd90af63759c7fed2416eaa342
Coco-hanqi <hanqi_zhang@berkeley.edu>
2021-10-27 19:21:23 -0700

    store keypoints into a csv file

--- mediapipe/static.py
+++ mediapipe/static.py
@@ -1,31 +1,75 @@
 import cv2
 import mediapipe
+import csv
+import os
 
 drawingModule = mediapipe.solutions.drawing_utils
 handsModule = mediapipe.solutions.hands
 
-with handsModule.Hands(static_image_mode=True) as hands:
-    image = cv2.imread(r"C:\Users\Xylon\OneDrive\Desktop\SLR\mediapipe\data\frame1.4.jpg")
+#  create a CSV file
+header = ['WRIST_p', 'WRIST_n', 'THUMB_CMC_p', 'THUMB_CMC_n',
+          'THUMB_MCP_p', 'THUMB_MCP_n', 'THUMB_IP_p', 'THUMB_IP_n',
+          'THUMB_TIP_p', 'THUMB_TIP_n', 'INDEX_FINGER_MCP_p', 'INDEX_FINGER_MCP_n',
+          'INDEX_FINGER_PIP_p', 'INDEX_FINGER_PIP_n', 'INDEX_FINGER_DIP_p', 'INDEX_FINGER_DIP_n',
+          'INDEX_FINGER_TIP_p','INDEX_FINGER_TIP_n', 'MIDDLE_FINGER_MCP_p', 'MIDDLE_FINGER_MCP_n',
+          'MIDDLE_FINGER_PIP_p', 'MIDDLE_FINGER_PIP_n', 'MIDDLE_FINGER_DIP_p', 'MIDDLE_FINGER_DIP_n',
+          'MIDDLE_FINGER_TIP_p', 'MIDDLE_FINGER_TIP_n', 'RING_FINGER_MCP_p', 'RING_FINGER_MCP_n',
+          'RING_FINGER_PIP_p', 'RING_FINGER_PIP_n', 'RING_FINGER_DIP_p', 'RING_FINGER_DIP_n',
+          'RING_FINGER_TIP_p', 'RING_FINGER_TIP_n', 'PINKY_MCP_p', 'PINKY_MCP_n',
+          'PINKY_PIP_p', 'PINKY_PIP_n', 'PINKY_DIP_p', 'PINKY_DIP_n',
+          'PINKY_TIP_p', 'PINKY_TIP_n',
+          'WRIST_p', 'WRIST_n', 'THUMB_CMC_p', 'THUMB_CMC_n',
+          'THUMB_MCP_p', 'THUMB_MCP_n', 'THUMB_IP_p', 'THUMB_IP_n',
+          'THUMB_TIP_p', 'THUMB_TIP_n', 'INDEX_FINGER_MCP_p', 'INDEX_FINGER_MCP_n',
+          'INDEX_FINGER_PIP_p', 'INDEX_FINGER_PIP_n', 'INDEX_FINGER_DIP_p', 'INDEX_FINGER_DIP_n',
+          'INDEX_FINGER_TIP_p','INDEX_FINGER_TIP_n', 'MIDDLE_FINGER_MCP_p', 'MIDDLE_FINGER_MCP_n',
+          'MIDDLE_FINGER_PIP_p', 'MIDDLE_FINGER_PIP_n', 'MIDDLE_FINGER_DIP_p', 'MIDDLE_FINGER_DIP_n',
+          'MIDDLE_FINGER_TIP_p', 'MIDDLE_FINGER_TIP_n', 'RING_FINGER_MCP_p', 'RING_FINGER_MCP_n',
+          'RING_FINGER_PIP_p', 'RING_FINGER_PIP_n', 'RING_FINGER_DIP_p', 'RING_FINGER_DIP_n',
+          'RING_FINGER_TIP_p', 'RING_FINGER_TIP_n', 'PINKY_MCP_p', 'PINKY_MCP_n',
+          'PINKY_PIP_p', 'PINKY_PIP_n', 'PINKY_DIP_p', 'PINKY_DIP_n',
+          'PINKY_TIP_p', 'PINKY_TIP_n']
 
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+with open('keypoints.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
 
-    imageHeight, imageWidth, _ = image.shape
+    # read all images in the data folder
+    root_dir = r'C:\Users\Xylon\OneDrive\Desktop\SLR\mediapipe\data'
+    for subdir, dirs, files in os.walk(root_dir):
+        for file in files:
+            image_path = os.path.join(subdir, file)
+            data = []
+            with handsModule.Hands(static_image_mode=True) as hands:
+                image = cv2.imread(image_path)
 
-    if results.multi_hand_landmarks != None:
-        for handLandmarks in results.multi_hand_landmarks:
-            drawingModule.draw_landmarks(image, handLandmarks, handsModule.HAND_CONNECTIONS)
+                results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-            for point in handsModule.HandLandmark:
-                normalizedLandmark = handLandmarks.landmark[point]
-                pixelCoordinatesLandmark = drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x,
-                                                                                          normalizedLandmark.y,
-                                                                                          imageWidth, imageHeight)
+                imageHeight, imageWidth, _ = image.shape
 
-                print(point)
-                print(pixelCoordinatesLandmark)
-                print(normalizedLandmark)
+                if results.multi_hand_landmarks != None:
+                    for handLandmarks in results.multi_hand_landmarks:
+                        drawingModule.draw_landmarks(image, handLandmarks, handsModule.HAND_CONNECTIONS)
 
-    cv2.imshow('Test image', image)
+                        for point in handsModule.HandLandmark:
+                            normalizedLandmark = handLandmarks.landmark[point]
+                            pixelCoordinatesLandmark = drawingModule._normalized_to_pixel_coordinates(normalizedLandmark.x,
+                                                                                                      normalizedLandmark.y,
+                                                                                                      imageWidth, imageHeight)
 
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+                            # print(point)
+                            # print(pixelCoordinatesLandmark)
+                            # print(normalizedLandmark)
+                            data.append(pixelCoordinatesLandmark)
+                            normalized = (normalizedLandmark.x, normalizedLandmark.y, normalizedLandmark.z)
+                            data.append(normalized)
+
+                writer.writerow(data)
+                # cv2.imshow('Test image', image)
+                name = file.split(".")[0]
+                store_path = os.path.join(subdir, name + '_kp' + '.jpg')
+                print("saving keypoints for " + store_path)
+                cv2.imwrite(store_path, image)
+
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
